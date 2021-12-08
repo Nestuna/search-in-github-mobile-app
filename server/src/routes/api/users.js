9ab54d04dea78090e4fef03d91b15e7c8c5da1e9
nantunes <nicolas.antunes@ubicast.eu>
2021-12-08 17:33:50 +0100

    clean routes

--- server/src/routes/api/users.js
+++ server/src/routes/api/users.js
@@ -21,33 +21,8 @@ api.get('/:username', async (req, response) => {
   response.json({ data: { user } })
 })
 
-/** POST /api/users > Create an user */
-api.post('/:username', async (request, response) => {
-  try {
-    const newUser = await prisma.user.create({
-      data: {
-        email: request.body.email,
-        username: request.body.username
-      }
-    })
-    return response.json({data: {message: `User ${newUser.email} created`}})
-  } catch(e) {
-    console.error(`Failed to create user : ${e}`)
-    return response.json({data: {message: 'Failed to create user'}})
-  }
-})
-
 api.delete('/:username', async (request, response) => {
-  const { id  } = request.params
-  try {
-    await prisma.user.delete({
-      where: { id: parseInt(id) }
-    })
-    return response.json({data : {message: `user ${id} deleted`}})
-  } catch(e) {
-    console.error(`Failed user ${id}`)
-    return response.json({data: {message: `user ${id} deleted`}})
-  }
+  return response.json({ data: {} })
 })
 
 export default api
